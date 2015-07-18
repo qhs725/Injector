9c615ec4f1b6c7bc54dc5efb583d01086a2a2112
dannyfreeman <fgx556@mocs.utc.edu>
2015-07-18 15:03:16 -0400

    made a button work

--- index.js
+++ index.js
@@ -9,29 +9,47 @@ function dummy(text, callback) {
 exports.dummy = dummy;
 */
 
-var buttons = require('sdk/ui/button/action');
-var tabs = require("sdk/tabs");
+var { ToggleButton } = require('sdk/ui/button/toggle');
+var panels = require("sdk/panel");
+var sidePanel = require("sdk/panel");
+var self = require("sdk/self");
 
-var button = buttons.ActionButton({
-  id: "mozilla-link",
-  label: "Visit Mozilla",
-  icon: {
-    "16": "./icon-16.png",
-    "32": "./icon-32.png",
-    "64": "./icon-64.png"
-  },
-  onClick: handleClick
+var button = ToggleButton({
+    id: "my-button",
+    label: "my button",
+    icon: {
+        "16": "./icon-16.png",
+        "32": "./icon-32.png",
+        "64": "./icon-64.png"
+    },
+    onChange: handleChange
 });
 
-function handleClick(state) {
-	var tabs = require('sdk/tabs');
 
-tabs.on('activate', function () {
-	/* var child = document.createElement("div");
-document.body.appendChild(child); 
-document.body.style.backgroundColor = "#D93600";
-*/
-  console.log('active: ' + tabs.activeTab.url);
+
+// Data for panel
+var panel = panels.Panel({
+    width: 250,
+    height: 400,
+    contentURL: self.data.url("dropDownPanelOptions.html"),
+    onHide: handleHide
 });
-  
+
+/*var sidePanel = panels.Panel({
+    contentURL: self.data.url(""),
+    onHide: handleHide
+}); */
+
+
+// Actions for the buttons
+function handleChange(state) {
+    if (state.checked) {
+        panel.show({
+            position: button
+        });
+    }
+}
+
+function handleHide() {
+      button.state('window', {checked: false});
 }
